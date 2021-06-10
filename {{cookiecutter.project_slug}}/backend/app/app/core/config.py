654a43dd6090f02097505d14379e5a97c9708356
dpetkov <dpetkov@grandenergy.net>
2021-06-10 13:48:51 +0300

    final

--- {{cookiecutter.project_slug}}/backend/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/config.py
@@ -6,9 +6,9 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str = 'AzR05Uq8U-Jvu9S_NOEMg0evTIyitpQtC32_elmBHIo1m71RqFRmWV7OXescSoSu0N9KyGSV7lg6vZx31NM-Ya3rsVnIEjBfSkTsdGhnEnV79pr63-uZ4IN1TD9MRw0xe8Y9hpCdZn1BB1RvHbeat3FflbfR5flxdd8HZIpYu-Y' # secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 # 60 * 24 * 8
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
